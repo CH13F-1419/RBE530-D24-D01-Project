8e8529640dddc55da4c9f53d2d24ae59ca456d3e
Jessica M. Rhodes <97774889+JMRhodes03@users.noreply.github.com>
2024-04-24 10:14:01 -0400

    change

--- Alan_DCMotorTest/Alan_DCMotorTest.ino
+++ Alan_DCMotorTest/Alan_DCMotorTest.ino
@@ -7,7 +7,8 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Connect two motors with the following terminals
 Adafruit_DCMotor *motor1 = AFMS.getMotor(1); // Motor connected to M1
-// Adafruit_DCMotor *motor2 = AFMS.getMotor(2); // Motor connected to M2
+Adafruit_DCMotor *motor2 = AFMS.getMotor(2); // Motor connected to M2
+
 const int buttonPin = 2;  // the number of the pushbutton pin
 int buttonState = 0;
 
@@ -17,7 +18,7 @@ void setup() {
 
   AFMS.begin();  // Create with the default frequency 1.6KHz
   motor1->setSpeed(150);  // Set the speed to 150/255
- //motor2->setSpeed(150);  // Set the speed to 150/255
+  motor2->setSpeed(150);  // Set the speed to 150/255
  pinMode(buttonPin, INPUT);
 }
 
@@ -26,17 +27,17 @@ void loop() {
   if (buttonState == HIGH) {
   Serial.println("Forward!");
   motor1->run(FORWARD);
-  //motor2->run(FORWARD);
+  motor2->run(FORWARD);
   delay(1000);
   }
   else {
   Serial.println("Backward!");
   motor1->run(BACKWARD);
-  //motor2->run(BACKWARD);
+  motor2->run(BACKWARD);
   delay(1000);
   }
   Serial.println("Release");
   motor1->run(RELEASE);
-  //motor2->run(RELEASE);
+  motor2->run(RELEASE);
   delay(1000);
 }
